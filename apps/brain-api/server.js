f55d09034d4494bf7c9a9ddf5a83d093b472813e
Zackary Brown <sl0thm4n@MacBookPro.lan>
2025-08-13 17:22:12 -0400

    google: wire Sheets routes

--- apps/brain-api/server.js
+++ apps/brain-api/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import docsRoutes from './docs.js';
-
+import sheetsRoutes from './sheets.js';
 import googleRoutes from './google.js';
 import batteryRoutes from './battery.js';
 
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '2mb' }));
-
+app.use('/v1/google/sheets', sheetsRoutes);
 app.get('/health', (req, res) => res.json({ ok: true }));
 app.use('/v1/google/docs', docsRoutes);
 app.use('/v1/google', googleRoutes);
